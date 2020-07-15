be28e3451fe070f3fb677165d0ccde43fd4ce184
MaksFenek <61117413+MaksFenek@users.noreply.github.com>
2020-07-15 14:43:43 +0300

    master

--- main.js
+++ main.js
@@ -1,16 +1,71 @@
-
+'use strick'
 
 //Select element function
 const $ = function (selector) {
     return document.querySelector(selector);
 }
 
-let menuToggler = $('.menu-toggle');
-let body = $('body');
+const menuToggler = $('.menu-toggle');
+const body = $('body');
 
 menuToggler.addEventListener('click', () => {
     body.classList.toggle('open');
 })
+
+
+
+// Modals
+
+/* Explore button */
+const exploreBtn = $('.cta-btn')
+
+exploreBtn.addEventListener('click', () => {
+    Swal.fire(
+        "I'm sorry",
+        'But I have no idea what to add here',
+        'info'
+    )
+})
+/* About us button */
+const aboutBtn = $('.body-btn')
+
+aboutBtn.addEventListener('click', () => {
+    Swal.fire(
+        'More about us',
+        'We just a restaurant',
+        'success'
+    )
+})
+
+/* Menu button */
+const menuBtn = $('#menu-btn')
+
+menuBtn.addEventListener('click', () => {
+    Swal.fire(
+        'If you want to view our menu',
+        'Come to our restaurant',
+        'warning'
+    )
+})
+
+const reservationsBtn = $('#reservations-btn')
+
+reservationsBtn.addEventListener('click', function () {
+    (async () => {
+
+        const { value: email } = await Swal.fire({
+            title: 'Input email address',
+            input: 'email',
+            inputPlaceholder: 'Enter your email address'
+        })
+
+        if (email) {
+            Swal.fire(`Entered email: ${email}`)
+        }
+
+    })()
+})
+
 
 //Scroll reveal
 

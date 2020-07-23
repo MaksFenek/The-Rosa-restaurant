31973b0fdbb6629c121b5ea95e7eedc2ce218c33
MaksFenek <61117413+MaksFenek@users.noreply.github.com>
2020-07-23 10:20:11 +0300

    Custome scrollbar is deleted, modals was replaced by native modal

--- main.js
+++ main.js
@@ -12,58 +12,60 @@ menuToggler.addEventListener('click', () => {
     body.classList.toggle('open');
 })
 
-
+const links = document.querySelectorAll('a.nav-link');
+links.forEach((link) => {
+    link.addEventListener('click', function () {
+        body.classList.remove('open')
+    })
+})
 
 // Modals
 
+modal = $('.modal-overlay')
+closeBtn = $('.modal-window span')
+
+closeBtn.addEventListener('click', () => {
+    modal.style.opacity = '0'
+    modal.style.visibility = 'hidden'
+})
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.opacity = '0'
+        modal.style.visibility = 'hidden'
+    }
+}
 /* Explore button */
 const exploreBtn = $('.cta-btn')
 
 exploreBtn.addEventListener('click', () => {
-    Swal.fire(
-        "I'm sorry",
-        'But I have no idea what to add here',
-        'info'
-    )
+    modal.style.opacity = '1'
+    modal.style.visibility = 'visible'
 })
+
+
+
 /* About us button */
 const aboutBtn = $('.body-btn')
 
 aboutBtn.addEventListener('click', () => {
-    Swal.fire(
-        'More about us',
-        'We just a restaurant',
-        'success'
-    )
+    modal.style.opacity = '1'
+    modal.style.visibility = 'visible'
 })
 
 /* Menu button */
 const menuBtn = $('#menu-btn')
 
 menuBtn.addEventListener('click', () => {
-    Swal.fire(
-        'If you want to view our menu',
-        'Come to our restaurant',
-        'warning'
-    )
+    modal.style.opacity = '1'
+    modal.style.visibility = 'visible'
 })
 
 const reservationsBtn = $('#reservations-btn')
 
 reservationsBtn.addEventListener('click', function () {
-    (async () => {
-
-        const { value: email } = await Swal.fire({
-            title: 'Input email address',
-            input: 'email',
-            inputPlaceholder: 'Enter your email address'
-        })
-
-        if (email) {
-            Swal.fire(`Entered email: ${email}`)
-        }
-
-    })()
+    modal.style.opacity = '1'
+    modal.style.visibility = 'visible'
 })
 
 
